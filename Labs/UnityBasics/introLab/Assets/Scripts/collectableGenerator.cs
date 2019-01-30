a361364ea67a4b56892c1257f652f2b847287a80
Ant1ng <ant1ng@berkeley.edu>
2019-01-30 04:46:00 -0800

    Organization

---
+++ Labs/UnityBasics/introLab/Assets/Scripts/collectableGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectableGenerator : MonoBehaviour
+{
+    public GameObject pellet;
+    
+    void Start()
+    {
+        for (int i = 0; i < 300; i++)
+        {
+            Instantiate(pellet, new Vector3((Random.value - 0.5f) * 61.0f, (Random.value - 0.5f) * 29.0f, 0), Quaternion.identity);
+        }
+    }
+}
